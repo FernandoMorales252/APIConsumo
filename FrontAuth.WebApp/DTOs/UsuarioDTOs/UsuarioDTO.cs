b01829895ad1a8e2689190d3bec9407c0198159e
LUIS FERNANDO MORALES PEREZ <Fernandomoe09@outlook.com>
2025-09-16 19:36:03 -0600

    Agregar archivos de proyecto.

---
+++ FrontAuth.WebApp/DTOs/UsuarioDTOs/UsuarioDTO.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FrontAuth.WebApp.DTOs.UsuarioDTOs
+{
+    public class UsuarioDTO
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre es requerido")]
+        [StringLength(100, ErrorMessage = "El nombre no puede tener mas de 100 caracteres")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "El Email es requerido")]
+        [StringLength(100, ErrorMessage = "El Email no puede tener mas de 100 caracteres")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "La contraseña es requerida")]
+        [StringLength(100, MinimumLength = 5, ErrorMessage = "La contraseña debe tener entre 4 y 100 caracteres")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public string Rol { get; set; }
+    }
+}
---
+++ FrontAuth.WebApp/DTOs/UsuarioDTOs/UsuarioLoginDTO.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FrontAuth.WebApp.DTOs.UsuarioDTOs
+{
+    public class UsuarioLoginDTO
+    {
+        [Required(ErrorMessage = "El Email es obligatorio")]
+        [EmailAddress(ErrorMessage = "El Email no es correcto")]
+        [StringLength(100, ErrorMessage = "El Email no puede contener mas de 100 caracteres")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "La contraseña es requerida")]
+        [StringLength(100, MinimumLength = 5, ErrorMessage = "La contraseña debe tener entre 4 y 100 caracteres")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
